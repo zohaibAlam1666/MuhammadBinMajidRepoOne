14e2aadab40008cf1df6521d02ac2718581d69d0
zohaibAlam1666 <zohaib.alam@ycsols.local>
2025-09-05 18:44:20 +0500

    first

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("this is the new page")
+list = [1,2,4,5,6,7]
+
+squ = [x*x for x in list]
+
+print(squ)
+
+import itertools
+
+def fib(n):
+     a, b = 0, 1
+
+     for _ in range(n):
+          yield a
+          a, b = b, a + b
+
+b = fib(20)
+
+print(next(b))
+
+print(next(b))
+
+newseries = list(itertools.islice(b,14))
+
+
+class Bank:
+
+    def __init__(self, owner, balance=0):
+          self.owner = owner
+          self.balance = balance
+          self.newValue = "nenenen"
+
+    
+
+
+
+    def  depost(self, parFirst):
+
+         print(f"balance {self.balance}")        
+
+
+account = Bank("Ali", 10000)
+
+account.depost("skldjfkllsdj")
